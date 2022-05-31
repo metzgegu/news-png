755c7f5218a4f2497d8ffb99843c560422c5038f
Guillaume Metzger <31687635+metzgegu@users.noreply.github.com>
2022-05-31 18:15:18 +0200

    Remove useless assets of this branch

--- index.js
+++
@@ -1,7 +0,0 @@
-const { draw } = require("./src/draw");
-const { getNews } = require("./src/rss");
-
-(async () => {
-    const news = await getNews()
-    draw(news.items.map(item => item.title))
-})();
--- src/rss.js
+++
@@ -1,7 +0,0 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
-
-exports.getNews = async () => {
-    return await parser.parseURL('https://www.lemonde.fr/international/rss_full.xml');
-}
-
